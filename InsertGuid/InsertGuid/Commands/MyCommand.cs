48bbc3d3167b4d5a44d321d12701ae32647db282
Sebastian Jensen <apps@tsjdev-apps.de>
2022-02-24 18:20:25 +0100

    Add project with the VS extension to insert a GUID.

---
+++ InsertGuid/InsertGuid/Commands/MyCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace InsertGuid
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var documentView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = documentView.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if (selection != null)
+                documentView.TextBuffer.Replace(selection, Guid.NewGuid().ToString());
+            else
+                documentView.TextBuffer.Insert(documentView.TextView.Caret.Position.BufferPosition,
+                    Guid.NewGuid().ToString());
+        }
+    }
+}
